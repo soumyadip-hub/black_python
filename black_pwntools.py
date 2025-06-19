8df7ed61556129e963578f454322d84456bf8f90
noobtolearner <soumyadippakhira094@gmail.com>
2025-06-19 21:32:54 +0530

    learn about pwn_tools

---
+++ black_pwntools.py
@@ -0,0 +1,13 @@
+from pwn import *
+print(cyclic(50))
+print(cyclic_find("laaa"))
+
+print(shellcraft.sh())
+print(hexdump(asm(shellcraft.sh())))
+# use for remote networking communication
+r = remote("127.0.0.1", 1234)
+r.sendline("hello!")
+r.interactive()
+r.close()
+
+# for more information doing research on https://docs.pwntools.com/en/stable/ in this website
