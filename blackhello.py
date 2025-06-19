4fb2b30e25487a69ace560c406e167f9310a4106
noobtolearner <soumyadippakhira094@gmail.com>
2025-06-19 20:04:19 +0530

    learn about import system and import requests

--- blackhello.py
+++ blackhello.py
@@ -1,2 +1,25 @@
-print("HELLO :) FOOLSSSSS")
-print("this the start of blackhat python.....")
+import sys
+
+print(sys.version)
+print(sys.executable)
+print(sys.platform)
+
+# -----------introduction to request-opendir---
+# import request use for web application exploitation=---
+
+
+import requests
+
+x = requests.get('http://httpbin.org/get')
+
+print(x.headers)
+print(x.headers['Server'])
+print(x.status_code)
+
+if x.status_code == 200:
+    print("Sucess!")
+elif x.status_code == 404:
+    print("Not found")
+
+print(x.elapsed)
+print(x.cookies)
