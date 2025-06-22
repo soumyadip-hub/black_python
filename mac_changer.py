5d16cd34f034c814d28f8a0ac073d7fa2af36c86
noobtolearner <soumyadippakhira094@gmail.com>
2025-06-22 23:38:02 +0530

    learn about mac cahnging method2

--- mac_changer.py
+++ mac_changer.py
@@ -1,6 +1,19 @@
 # type: ignore
 import subprocess
-subprocess.call("ifconfig", shell=True) 
-subprocess.call("ifconfig wlan0 down", shell=True)
-subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:55:66", shell=True)
-subprocess.call("ifconfig wlan0 up", shell=True)
+# ---------method2-----------------
+interface = input("interface > ")
+new_mac = input("new Mac > ")
+
+print("[+] Changing your mac adress for " + interface + " to " + new_mac)
+
+subprocess.call("ifconfig " + interface +" down ", shell=True)
+subprocess.call("ifconfig " + interface +" hw ether " + new_mac, shell=True)
+subprocess.call("ifconfig " + interface +" up ", shell=True)
+
+
+
+# -------------------------------------method1--------
+# subprocess.call("ifconfig", shell=True) 
+# subprocess.call("ifconfig wlan0 down", shell=True)
+# subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:55:66", shell=True)
+# subprocess.call("ifconfig wlan0 up", shell=True)
